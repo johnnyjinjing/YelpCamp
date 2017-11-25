dda9ff8ce547d8a99a7b88c3239dadb2895c326a
Jing Jin <johnnyjinjing@gmail.com>
2017-11-25 14:32:47 -0800

    add user auth

--- app.js
+++ app.js
@@ -1,37 +1,65 @@
-var express = require("express"),
+var express = require('express'),
     app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local');
 
-var Campground = require("./models/campground"),
-    Comment = require("./models/comment");
+var Campground = require('./models/campground'),
+    Comment = require('./models/comment'),
+    User = require('./models/user');
 
-var seedDB = require("./seeds");
+var seedDB = require('./seeds');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/yelp_camp", { useMongoClient: true });
+mongoose.connect('mongodb://localhost/yelp_camp', { useMongoClient: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 seedDB();
 
-app.get("/", function(req, res) {
-    res.render("landing");
+// passport config
+app.use(
+    require('express-session')({
+        secret: 'ahdadhkadhsdashasd',
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.user = req.user;
+    next();
 });
 
-app.get("/campgrounds", function(req, res) {
+// routes
+app.get('/', function(req, res) {
+    res.render('landing');
+});
+
+// campground index page
+app.get('/campgrounds', function(req, res) {
     Campground.find({}, function(err, campgrounds) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds/index", { campgrounds: campgrounds });
+            res.render('campgrounds/index', {
+                campgrounds: campgrounds
+            });
         }
     });
 });
 
-app.post("/campgrounds", function(req, res) {
+// post new campground
+app.post('/campgrounds', function(req, res) {
     var newCampground = {
         name: req.body.name,
         image: req.body.image,
@@ -41,45 +69,48 @@ app.post("/campgrounds", function(req, res) {
         if (err) {
             console.log(err);
         } else {
-            console.log("Create new Campground:");
+            console.log('Create new Campground:');
             console.log(campground);
-            res.redirect("/campgrounds");
+            res.redirect('/campgrounds');
         }
     });
 });
 
-app.get("/campgrounds/new", function(req, res) {
-    res.render("campgrounds/new");
+// form to add a new campground
+app.get('/campgrounds/new', function(req, res) {
+    res.render('campgrounds/new');
 });
 
-// SHOW
-app.get("/campgrounds/:id", function(req, res) {
+// single campground
+app.get('/campgrounds/:id', function(req, res) {
     Campground.findById(req.params.id)
-        .populate("comments")
+        .populate('comments')
         .exec(function(err, campground) {
             if (err) {
                 console.log(err);
             } else {
-                res.render("campgrounds/show", { campground: campground });
+                res.render('campgrounds/show', { campground: campground });
             }
         });
 });
 
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+// new comment form
+app.get('/campgrounds/:id/comments/new', isLoggedIn, function(req, res) {
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
         } else {
-            res.render("comments/new", { campground: campground });
+            res.render('comments/new', { campground: campground });
         }
     });
 });
 
-app.post("/campgrounds/:id/comments/", function(req, res) {
+// post a comment
+app.post('/campgrounds/:id/comments/', isLoggedIn, function(req, res) {
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
-            res.redirect("/campgrounds");
+            res.redirect('/campgrounds');
         } else {
             Comment.create(req.body.comment, function(err, comment) {
                 if (err) {
@@ -87,13 +118,56 @@ app.post("/campgrounds/:id/comments/", function(req, res) {
                 } else {
                     campground.comments.push(comment);
                     campground.save();
-                    res.redirect("/campgrounds/" + campground._id);
+                    res.redirect('/campgrounds/' + campground._id);
                 }
             });
         }
     });
 });
 
+// auth routes
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+app.post('/register', function(req, res) {
+    var newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req, res, function() {
+            res.redirect('/campgrounds');
+        });
+    });
+});
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+app.post(
+    '/login',
+    passport.authenticate('local', {
+        successRedirect: '/campgrounds',
+        failureRedirect: '/login'
+    }),
+    function(req, res) {}
+);
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/campgrounds');
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
 app.listen(3000, function() {
-    console.log("The YelpCamp Server has started...");
+    console.log('The YelpCamp Server has started...');
 });
